2ba8de88624631c92ab5b1816db5809a4f93bc7e
xxqLgnd <yvangengeryt@mail.ru>
2025-07-12 17:42:47 +0300

    Update

--- Access.py
+++ Access.py
@@ -3,7 +3,7 @@
   "xxqLgnd_E5f6G7h8": "???",
   "xxqLgnd_i9J0kL1m": "???",
   "xxqLgnd_N2o3P4q5": "???",
-  "xxqLgnd_R6s7T8u9": "",
+  "xxqLgnd_R6s7T8u9": "5DA1B1EC-05DA-9B17-AC6E-345A6019A4EE",
   "xxqLgnd_V0w1X2y3": "6659A1A8-1E57-0000-0000-000000000000",
   "xxqLgnd_Z4a5B6c7": "???",
   "xxqLgnd_D8e9F0g1": "???"
