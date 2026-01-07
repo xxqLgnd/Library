1b4649c1927fcf623624eaa313bd7b685254e913
xxqLgnd <yvangengeryt@mail.ru>
2026-01-07 03:15:15 +0300

    Update

--- Access.py
+++ Access.py
@@ -1,10 +1,10 @@
 {
-  "xxqLgnd_a1B2c3D4": "16FFEA1FB867",
+  "xxqLgnd_a1B2c3D4": "???",
   "xxqLgnd_E5f6G7h8": "A8A15966571E",
   "xxqLgnd_i9J0kL1m": "???",
   "xxqLgnd_N2o3P4q5": "???",
-  "xxqLgnd_R6s7T8u9": "5DA1B1EC-05DA-9B17-AC6E-345A6019A4EE",
-  "xxqLgnd_V0w1X2y3": "6659A1A8-1E57-0000-0000-000000000000",
+  "xxqLgnd_R6s7T8u9": "???",
+  "xxqLgnd_V0w1X2y3": "???",
   "xxqLgnd_Z4a5B6c7": "???",
   "xxqLgnd_D8e9F0g1": "???"
 }
